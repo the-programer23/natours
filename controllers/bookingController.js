927e3f3ab03626cce6301ab828a801841f9c3547
Fabian Pinzon <nfabian.pm@gmail.com>
2020-04-04 18:21:35 -0500

    Improved Stripe implementation 2

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -22,7 +22,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
         line_items: [{
             name: `Tour ${tour.name}`,
             description: tour.summary,
-            images: [`https://www.natours.dev/img/tours/${tour.imageCover}`],
+            images: [`${req.protocol}://${req.get('host')}/img/tours/${tour.imageCover}`],
             //this in in cents if price is 1 dolar then it is equals to 100 cents
             amount: tour.price * 100,
             currency: 'usd',
@@ -60,7 +60,7 @@ const createBookingCheckout = async (session) => {
     const user = await (User.findOne({
         email: session.customer_email
     })).id;
-    const price = session.line_items[0].amount / 100;
+    const price = session.display_items[0].amount / 100;
 
     await Booking.create({
         tour,
@@ -83,7 +83,7 @@ exports.webhookCheckout = (req, res, next) => {
         return res.status(400).send(`Webhook error: ${err.message}`)
     }
 
-    if (event.type === 'checkout.session.complete')
+    if (event.type === 'checkout.session.completed')
         createBookingCheckout(event.data.object)
 
     res.status(200).json({
